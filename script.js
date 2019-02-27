02e2ed2b5a842f5491396ba869d78513edcf739f
Matthew Nash <nashma4@gmail.com>
2019-02-27 17:33:31 -0500

    improved logging, added stacking indicator

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ if (localStorage.getItem("log")) {
 else {
     var log = [];
     localStorage.setItem("log", JSON.stringify(log));
+    parseLog(log);
 }
 
 var icr = document.getElementById('icr').value,
@@ -45,33 +46,71 @@ function addCarbs() {
     return carbs;
 }
 
+function checkStacking() {
+    if (!JSON.parse(localStorage.getItem("log")).length) return false;
+    var d = new Date();
+    var M = d.getMonth() * 43800;
+    var D = d.getDate() * 1440;
+    var h = d.getHours() * 60;
+    var m = d.getMinutes();
+    var thisTime = M+ D+ h + m;
+
+    var logArray = JSON.parse(localStorage.getItem("log"));
+    var lastMonth = logArray[logArray.length-1].month * 43800;
+    var lastDay = logArray[logArray.length-1].date * 1440;
+    var lastHour = logArray[logArray.length-1].hour * 60;
+    var lastMinute = logArray[logArray.length-1].minute;
+    var lastTime = lastMonth + lastDay + lastHour + lastMinute;
+
+    var threeHours = 180;
+    var diff = lastTime + threeHours;
+
+    if (diff >= thisTime) return threeHours - (thisTime - lastTime);
+    else return false;
+}
+
 setInterval(function() {
-    var carbs = document.getElementById('carbs').value,
+    var carbs = addCarbs(),
         bg = document.getElementById('bg').value,
         icr = document.getElementById('icr').value,
         isf = document.getElementById('isf').value,
         target = parseInt(document.getElementById('target').value);
+
+    var lcarbs = localStorage.getItem("carbs"),
+        lbg = localStorage.getItem("bg"),
+        licr = localStorage.getItem("icr"),
+        lisf = localStorage.getItem("isf"),
+        ltarget = localStorage.getItem("target");
 
     // save ratios locally
     localStorage.setItem("icr", icr);
     localStorage.setItem("isf", isf);
     localStorage.setItem("target", target);
     localStorage.setItem("bg", bg);
-
-    // set current BGL placeholder
-    if (isNaN(target) == false) document.getElementById('bg').placeholder = "if > " + target;
-    else document.getElementById('bg').placeholder = "";
-
-    //add up carbs
-    var carbs = addCarbs();
-
-    if (carbs == 0) localStorage.setItem("carbs", "");
+    if (!carbs) localStorage.setItem("carbs", "");
     else localStorage.setItem("carbs", carbs);
 
+    // set current BGL placeholder
+    if (target) document.getElementById('bg').placeholder = "if > " + target;
+    else document.getElementById('bg').placeholder = "";
+
     //use ratios to calulate dose
-    if (carbs != null && bg > target) var dose = ((bg - target) / isf) + (carbs / icr);
-    else if (carbs != null) var dose = carbs / icr;
+    if (carbs && bg > target) var dose = ((bg - target) / isf) + (carbs / icr);
+    else if (carbs) var dose = carbs / icr;
     else if (bg > target) var dose = (bg - target) / isf;
+    else var dose = 0;
+
+    // check for stacking
+    if (checkStacking()) {
+        document.getElementById('dose').style.color = "orange";
+        document.getElementById('stacking').innerHTML = "To avoid insulin stacking, wait " + checkStacking() + " minutes before taking another dose.";
+        document.getElementById('stacking').style.display = "block";
+    }
+    else {
+        document.getElementById('dose').style.color = "blue";
+        document.getElementById('stacking').innerHTML = "";
+        document.getElementById('stacking').style.display = "none";
+    }
 
     document.getElementById('dose').value = Math.round(dose);
     document.getElementById('unrounded').innerHTML = dose;
@@ -128,17 +167,18 @@ function parseLog(o) {
         z.appendChild(t);
         y.appendChild(z);
     }
-    log.appendChild(y);
+    if (o.length > 0) log.appendChild(y);
+    else log.innerHTML = "No log items...";
 
     // populate log
-    // o.sort(function(a, b){return o[a].id - o[b].id});
+    o.reverse();
     for (i = 0; i < o.length; i++) {
 
       var y = document.createElement("TR");
       y.setAttribute("class", "logItem");
       y.setAttribute("id", "logItem"+o[i].id);
 
-      var tableItems = [o[i].dose, o[i].carbs, o[i].bg, o[i].icr, o[i].isf, o[i].target, addZero(o[i].hour) + ":" + addZero(o[i].minute) + ":" + addZero(o[i].second) + ", " + addZero(o[i].month) + "/" + addZero(o[i].date) + "/" + o[i].year, "<a href='javascript:deleteLogItem("+ o[i].id + ");'>delete</a>"];
+      var tableItems = [o[i].dose, o[i].carbs, o[i].bg, o[i].icr, o[i].isf, o[i].target, addZero(o[i].hour) + ":" + addZero(o[i].minute) + ":" + addZero(o[i].second) + "\u000a" + addZero(o[i].month) + "/" + addZero(o[i].date) + "/" + String(o[i].year).substring(2), "<a href='javascript:deleteLogItem("+ o[i].id + ");'>delete</a>"];
 
       for (item in tableItems) {
           var z = document.createElement("TD");
@@ -189,12 +229,12 @@ function log() {
 
     var log = JSON.parse(localStorage.getItem("log"));
 
-    if (log.length > 1 && dose.id == log[log.length-1].id) return false;
+    if (log.length > 0 && dose.id == log[log.length-1].id) return false;
     else {
         // add log item
         log.push(dose);
-        parseLog(log);
         localStorage.setItem("log", JSON.stringify(log));
+        parseLog(log);
 
         // clear fields
         var carbs = document.querySelectorAll('.carb');
