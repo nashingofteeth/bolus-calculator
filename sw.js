f3d48e1eb2950e095ebf94b7e16a99691754fba9
nashingofteeth <260189+nashingofteeth@users.noreply.github.com>
2024-12-25 14:50:45 -0800

    fix PWA paths

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'bolus-calculator-v4.5.0';
+const CACHE_NAME = 'bolus-calculator-v4.5.1';
 const ASSETS = [
-  '/index.html',
-  '/script.js',
-  '/manifest.json',
-  '/assets/favicon.png',
-  '/assets/apple_icon.png',
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json',
+  './assets/favicon.png',
+  './assets/apple_icon.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css'
 ];
